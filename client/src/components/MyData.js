88ca70ceab4332466240fe0026fedaaef0e19321
Deepesh Kurmi <deepesh.kurmi@peakmind.in>
2021-11-14 17:20:01 +0530

    little change

--- client/src/components/MyData.js
+++ client/src/components/MyData.js
@@ -4,8 +4,7 @@ import ShowTable from './ShowTable';
 import StraightLine from './StraightLine';
 import LinkButton from './LinkButton';
 const MyData = () => {
-    const [data, setData] = useState([{"planogramName": "planogram1", "data": [{"category": "orange", "color": "#b6a4a4", "points": ["0,1"]}]}, {"planogramName": "planogram2", "data": [{"category": "orange", "color": "#f59132", "points": ["0,2", "1,1"]}]}, {"planogramName": "planogram3", "data": [{"category": "blue", "color": "#816db0", "points": ["0,2", "1,2", "2,1"]}]}]); 
-    // const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
     const handleData = async()=>{
         const res  = await axios.get("http://localhost:8000/mydata/");
         setData(res.data);
